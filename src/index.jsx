8ca1c634aefaa31b85002e49adf9c8114d878498
Pedro Godoy <pedro.godoy.simon@gmail.com>
2023-09-11 15:52:23 -0300

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const API_URL = "https://api.magicthegathering.io/v1/cards";
+
+const CardList = () => {
+  const [cards, setCards] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [cardInFocus, setCardInFocus] = useState(null);
+
+  const fetchCards = async () => {
+    const result = await fetch(`${API_URL}?name=${searchTerm}`, {
+      method: "GET",
+    });
+
+    const data = await result.json();
+
+    const names = new Set();
+
+    const filteredCards = data.cards.filter((card) => {
+      if (names.has(card.name)) {
+        return false;
+      }
+
+      names.add(card.name);
+      return true;
+    });
+
+    console.log(filteredCards)
+
+    setCards(filteredCards);
+  };
+
+  useEffect(() => {
+    fetchCards();
+  }, []);
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetchCards();
+        }}
+      >
+        <input onChange={(e) => setSearchTerm(e.target.value)} />
+        <button type="submit">Search</button>
+      </form>
+      {cardInFocus && <img src={cardInFocus.imageUrl} />}
+      <div>
+        {cards.map((card) => (
+          <button key={card.id} onClick={() => setCardInFocus(card)}>{card.name}</button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CardList;
